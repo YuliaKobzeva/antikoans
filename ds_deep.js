a7f60e4687915a046fb46e32781ade97bfe6f75c
Yuliya Kobzeva <lomeyka@gmail.com>
2021-02-24 22:35:32 +0300

    #5

--- ds_deep.js
+++ ds_deep.js
@@ -1,9 +1,26 @@
 const DS_DEEP = {
 	things: [
-		{ a: 1, b: [1, 2, 3], c: { k1: 1, k2: 2, k3: 3 } },
-		{ a: 2, b: [4, 5, 6], c: { k1: 4, k2: 5, k3: 6 } },
-		{ a: 3, b: [7, 8, 9], c: { k1: 7, k2: 8, k3: 9 }, i: "should not be here" },
-		{ a: 4, b: [10, 11, 12], c: { k1: 10, k2: 11, k3: 12 } },
+		{
+			a: 1,
+			b: [1, 2, 3],
+			c: { k1: 1, k2: 2, k3: 3 },
+		},
+		{
+			a: 2,
+			b: [4, 5, 6],
+			c: { k1: 4, k2: 5, k3: 6 },
+		},
+		{
+			a: 3,
+			b: [7, 8, 9],
+			c: { k1: 7, k2: 8, k3: 9 },
+			i: "should not be here",
+		},
+		{
+			a: 4,
+			b: [10, 11, 12],
+			c: { k1: 10, k2: 11, k3: 12 },
+		},
 	],
 
 	mouse: {
@@ -28,11 +45,9 @@ function destr1(obj) {
 	const { things } = obj;
 	if (things != null && things != undefined) {
 		return things;
+	} else {
+		return "no things in this object";
 	}
-
-    else {
-        return "no things in this object";
-    }
 }
 
 console.log("task 1:", destr1(DS_DEEP));
@@ -40,14 +55,14 @@ console.log("task 1:", destr1(DS_DEEP));
 //task 2
 
 function destr2(obj) {
-	const { things: [, second] } = obj;
+	const {
+		things: [, second],
+	} = obj;
 	if (second != null && second != undefined) {
 		return second;
+	} else {
+		return "no second in this object";
 	}
-
-    else {
-        return "no second in this object";
-    }
 }
 
 console.log("task 2:", destr2(DS_DEEP));
@@ -55,14 +70,14 @@ console.log("task 2:", destr2(DS_DEEP));
 //task 3
 
 function destr3(obj) {
-	const { things: [first, second] } = obj;
+	const {
+		things: [first, second],
+	} = obj;
 	if (second != null && second != undefined) {
 		return [second, first];
+	} else {
+		return "no second in this object";
 	}
-
-    else {
-        return "no second in this object";
-    }
 }
 
 console.log("task 3:", destr3(DS_DEEP));
@@ -71,12 +86,51 @@ console.log("task 3:", destr3(DS_DEEP));
 
 function destr4(obj) {
 	const { things } = obj;
-	return things.slice(-2, -1)
+	return things.slice(-2, -1);
 }
 
 console.log("task 4:", destr4(DS_DEEP));
 
+//task 5
+
+function destr5(obj) {
+	const { things: [, secondObject] } = obj;
+	const { b } = secondObject;
+
+	return b.splice(1);
+}
+
+console.log("task 5:", destr5(DS_DEEP));
+
+//task 6
 
 
+function destr6(obj, gluedKeys) {
+	const unGluedKeys = gluedKeys.split(".");
+	const lastElem = unGluedKeys.splice(-1); //["dog"]
+
+	const { mouse: {cat} } = obj;
+	const nessKey = Object.keys(cat) //["dog"]
+
+	// for (let i = 0; i < lastElem.length; i++) {
+
+	// 	if(lastElem[i] === nessKey[i]) {
+	// 		console.log("=");
+	// 	}
+	// }
+
+	// return lastElem
+
+}
+
+console.log("task 6:", destr6(DS_DEEP, "mouse.cat.dog"));
+
+/*
+	1) разбить строку по точке на массив.
+		получилось ["mouse", "cat", "dog"]
+	2) у массива последний элемент деструктурировать
+	3) вывести содержимое последнего элемента
+
+*/
 
 
